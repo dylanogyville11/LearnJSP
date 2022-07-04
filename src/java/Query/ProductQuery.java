1cfb62ec5493657d455f2e4e5102b36b7ad18887
dylanogyville11 <dylanogyville21@gmail.com>
2022-07-04 11:05:38 +0700

    first commit

---
+++ src/java/Query/ProductQuery.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Query;
+
+import Helper.StringHelper;
+import java.sql.ResultSet;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ *
+ * @author Dylan
+ */
+public class ProductQuery {
+    public String get = "SELECT * FROM product";
+    public String create = "INSERT INTO product (name, product_type, " + 
+            "stock, price, expired) VALUES (?, ?, ?, ?, ?)";
+    public String getByName = "SELECT * FROM product where name LIKE ?";
+    
+}
